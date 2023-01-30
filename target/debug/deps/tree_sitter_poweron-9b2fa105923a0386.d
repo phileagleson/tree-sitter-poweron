29bd997d72d373869627d2d0a55474a2ff97e5ed
Phil Eagleson <phileagleson@icloud.com>
2023-01-29 22:07:20 -0500

    updated build.rs to include scanner.cc

---
+++ target/debug/deps/tree_sitter_poweron-9b2fa105923a0386.d
@@ -0,0 +1,6 @@
+/Users/phil/projects/tree-sitter-poweron/target/debug/deps/tree_sitter_poweron-9b2fa105923a0386.rmeta: bindings/rust/lib.rs bindings/rust/../../src/node-types.json
+
+/Users/phil/projects/tree-sitter-poweron/target/debug/deps/tree_sitter_poweron-9b2fa105923a0386.d: bindings/rust/lib.rs bindings/rust/../../src/node-types.json
+
+bindings/rust/lib.rs:
+bindings/rust/../../src/node-types.json:
---
+++ target/debug/deps/tree_sitter_poweron-d792ea68c4939a81.d
@@ -0,0 +1,6 @@
+/Users/phil/projects/tree-sitter-poweron/target/debug/deps/tree_sitter_poweron-d792ea68c4939a81.rmeta: bindings/rust/lib.rs bindings/rust/../../src/node-types.json
+
+/Users/phil/projects/tree-sitter-poweron/target/debug/deps/tree_sitter_poweron-d792ea68c4939a81.d: bindings/rust/lib.rs bindings/rust/../../src/node-types.json
+
+bindings/rust/lib.rs:
+bindings/rust/../../src/node-types.json:
