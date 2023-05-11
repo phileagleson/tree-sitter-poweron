2fcfe38f24076dd288605bbcb8bb8fdb8431a29f
Phil Eagleson <phileagleson1@gmail.com>
2023-05-10 22:41:03 -0400

    Updated wasm bindings

---
+++ target/package/tree-sitter-poweron-1.0.0/target/debug/deps/cc-d1692f086c363dfb.d
@@ -0,0 +1,8 @@
+/home/phil/projects/tree-sitter-poweron/target/package/tree-sitter-poweron-1.0.0/target/debug/deps/cc-d1692f086c363dfb.rmeta: /home/phil/.cargo/registry/src/github.com-1ecc6299db9ec823/cc-1.0.79/src/lib.rs /home/phil/.cargo/registry/src/github.com-1ecc6299db9ec823/cc-1.0.79/src/windows_registry.rs
+
+/home/phil/projects/tree-sitter-poweron/target/package/tree-sitter-poweron-1.0.0/target/debug/deps/libcc-d1692f086c363dfb.rlib: /home/phil/.cargo/registry/src/github.com-1ecc6299db9ec823/cc-1.0.79/src/lib.rs /home/phil/.cargo/registry/src/github.com-1ecc6299db9ec823/cc-1.0.79/src/windows_registry.rs
+
+/home/phil/projects/tree-sitter-poweron/target/package/tree-sitter-poweron-1.0.0/target/debug/deps/cc-d1692f086c363dfb.d: /home/phil/.cargo/registry/src/github.com-1ecc6299db9ec823/cc-1.0.79/src/lib.rs /home/phil/.cargo/registry/src/github.com-1ecc6299db9ec823/cc-1.0.79/src/windows_registry.rs
+
+/home/phil/.cargo/registry/src/github.com-1ecc6299db9ec823/cc-1.0.79/src/lib.rs:
+/home/phil/.cargo/registry/src/github.com-1ecc6299db9ec823/cc-1.0.79/src/windows_registry.rs:
---
+++ target/package/tree-sitter-poweron-1.0.0/target/debug/deps/tree_sitter_poweron-9a6d32bf0f2e4ed8.d
@@ -0,0 +1,8 @@
+/home/phil/projects/tree-sitter-poweron/target/package/tree-sitter-poweron-1.0.0/target/debug/deps/tree_sitter_poweron-9a6d32bf0f2e4ed8.rmeta: bindings/rust/lib.rs bindings/rust/../../src/node-types.json
+
+/home/phil/projects/tree-sitter-poweron/target/package/tree-sitter-poweron-1.0.0/target/debug/deps/libtree_sitter_poweron-9a6d32bf0f2e4ed8.rlib: bindings/rust/lib.rs bindings/rust/../../src/node-types.json
+
+/home/phil/projects/tree-sitter-poweron/target/package/tree-sitter-poweron-1.0.0/target/debug/deps/tree_sitter_poweron-9a6d32bf0f2e4ed8.d: bindings/rust/lib.rs bindings/rust/../../src/node-types.json
+
+bindings/rust/lib.rs:
+bindings/rust/../../src/node-types.json:
