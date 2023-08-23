ebc535824764c51d590ac8c55b001553a0b97f68
Phil Eagleson <phileagleson1@gmail.com>
2023-08-23 13:40:30 -0400

    Updated foreach loops with optional until clause

---
+++ target/package/tree-sitter-poweron-1.0.2/target/debug/deps/tree_sitter_poweron-eead036065ff31f1.d
@@ -0,0 +1,8 @@
+/home/phil/projects/tree-sitter-poweron/target/package/tree-sitter-poweron-1.0.2/target/debug/deps/tree_sitter_poweron-eead036065ff31f1.rmeta: bindings/rust/lib.rs bindings/rust/../../src/node-types.json
+
+/home/phil/projects/tree-sitter-poweron/target/package/tree-sitter-poweron-1.0.2/target/debug/deps/libtree_sitter_poweron-eead036065ff31f1.rlib: bindings/rust/lib.rs bindings/rust/../../src/node-types.json
+
+/home/phil/projects/tree-sitter-poweron/target/package/tree-sitter-poweron-1.0.2/target/debug/deps/tree_sitter_poweron-eead036065ff31f1.d: bindings/rust/lib.rs bindings/rust/../../src/node-types.json
+
+bindings/rust/lib.rs:
+bindings/rust/../../src/node-types.json:
