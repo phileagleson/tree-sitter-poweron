46c93e06126249c7b13ec85091353c013940b877
Phil Eagleson <phileagleson1@gmail.com>
2023-08-23 13:44:32 -0400

    Updated Cargo crate

---
+++ target/package/tree-sitter-poweron-1.0.3/target/debug/deps/tree_sitter_poweron-29f9ebb70e48db2e.d
@@ -0,0 +1,8 @@
+/home/phil/projects/tree-sitter-poweron/target/package/tree-sitter-poweron-1.0.3/target/debug/deps/tree_sitter_poweron-29f9ebb70e48db2e.rmeta: bindings/rust/lib.rs bindings/rust/../../src/node-types.json
+
+/home/phil/projects/tree-sitter-poweron/target/package/tree-sitter-poweron-1.0.3/target/debug/deps/libtree_sitter_poweron-29f9ebb70e48db2e.rlib: bindings/rust/lib.rs bindings/rust/../../src/node-types.json
+
+/home/phil/projects/tree-sitter-poweron/target/package/tree-sitter-poweron-1.0.3/target/debug/deps/tree_sitter_poweron-29f9ebb70e48db2e.d: bindings/rust/lib.rs bindings/rust/../../src/node-types.json
+
+bindings/rust/lib.rs:
+bindings/rust/../../src/node-types.json:
