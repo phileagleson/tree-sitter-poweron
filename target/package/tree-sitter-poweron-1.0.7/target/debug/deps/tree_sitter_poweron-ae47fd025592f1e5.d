cbab27b60486da892a20b52dc1f9f9b8af90828d
Phil Eagleson <phileagleson1@gmail.com>
2023-08-29 15:34:54 -0400

    forgot to generate new wasm file

---
+++ target/package/tree-sitter-poweron-1.0.7/target/debug/deps/tree_sitter_poweron-ae47fd025592f1e5.d
@@ -0,0 +1,8 @@
+/home/phil/projects/tree-sitter-poweron/target/package/tree-sitter-poweron-1.0.7/target/debug/deps/tree_sitter_poweron-ae47fd025592f1e5.rmeta: bindings/rust/lib.rs bindings/rust/../../src/node-types.json
+
+/home/phil/projects/tree-sitter-poweron/target/package/tree-sitter-poweron-1.0.7/target/debug/deps/libtree_sitter_poweron-ae47fd025592f1e5.rlib: bindings/rust/lib.rs bindings/rust/../../src/node-types.json
+
+/home/phil/projects/tree-sitter-poweron/target/package/tree-sitter-poweron-1.0.7/target/debug/deps/tree_sitter_poweron-ae47fd025592f1e5.d: bindings/rust/lib.rs bindings/rust/../../src/node-types.json
+
+bindings/rust/lib.rs:
+bindings/rust/../../src/node-types.json:
