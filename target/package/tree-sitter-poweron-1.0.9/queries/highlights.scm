3db44c71651ce1388a887ccf73b86c1431c1deee
Phil Eagleson <phileagleson1@gmail.com>
2023-08-30 15:54:13 -0400

    updated rust crate

---
+++ target/package/tree-sitter-poweron-1.0.9/queries/highlights.scm
@@ -0,0 +1,31 @@
+; highlights.scm
+
+(comment) @comment
+(keyword) @keyword
+(target_division) @namespace
+(define_division) @namespace
+(setup_division) @namespace
+(print_division) @namespace
+(select_division) @namespace
+(sort_division) @namespace
+(letter_division) @namespace
+(total_division) @namespace
+(record_type) @type
+(include_statement)  @include
+(string_literal) @string
+(data_type) @type
+(array_type) @type
+(number) @number
+(procedure_call 
+  procedure_name: (identifier)) @function
+(procedure_definition) @keyword
+(while_statement) @repeat
+(for_statement) @repeat
+(if_statement) @conditional
+(date) @string
+(rate) @number
+(money) @string
+(identifier) @variable
+(for_loop) @repeat
+(poweron_function) @function
+(setexp) @function
---
+++ target/package/tree-sitter-poweron-1.0.9/target/debug/deps/tree_sitter_poweron-01f6b12aad2aa851.d
@@ -0,0 +1,8 @@
+/home/phil/projects/tree-sitter-poweron/target/package/tree-sitter-poweron-1.0.9/target/debug/deps/tree_sitter_poweron-01f6b12aad2aa851.rmeta: bindings/rust/lib.rs bindings/rust/../../src/node-types.json
+
+/home/phil/projects/tree-sitter-poweron/target/package/tree-sitter-poweron-1.0.9/target/debug/deps/libtree_sitter_poweron-01f6b12aad2aa851.rlib: bindings/rust/lib.rs bindings/rust/../../src/node-types.json
+
+/home/phil/projects/tree-sitter-poweron/target/package/tree-sitter-poweron-1.0.9/target/debug/deps/tree_sitter_poweron-01f6b12aad2aa851.d: bindings/rust/lib.rs bindings/rust/../../src/node-types.json
+
+bindings/rust/lib.rs:
+bindings/rust/../../src/node-types.json:
