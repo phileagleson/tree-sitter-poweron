9c2c9210ca846752bc270c52ad0403c5dc9468c1
Phil Eagleson <phileagleson1@gmail.com>
2023-09-08 15:51:21 -0400

    Updated rust crate

---
+++ target/package/tree-sitter-poweron-1.0.12/target/debug/deps/tree_sitter_poweron-c27d051a7c195563.d
@@ -0,0 +1,8 @@
+/home/phil/projects/tree-sitter-poweron/target/package/tree-sitter-poweron-1.0.12/target/debug/deps/tree_sitter_poweron-c27d051a7c195563.rmeta: bindings/rust/lib.rs bindings/rust/../../src/node-types.json
+
+/home/phil/projects/tree-sitter-poweron/target/package/tree-sitter-poweron-1.0.12/target/debug/deps/libtree_sitter_poweron-c27d051a7c195563.rlib: bindings/rust/lib.rs bindings/rust/../../src/node-types.json
+
+/home/phil/projects/tree-sitter-poweron/target/package/tree-sitter-poweron-1.0.12/target/debug/deps/tree_sitter_poweron-c27d051a7c195563.d: bindings/rust/lib.rs bindings/rust/../../src/node-types.json
+
+bindings/rust/lib.rs:
+bindings/rust/../../src/node-types.json:
