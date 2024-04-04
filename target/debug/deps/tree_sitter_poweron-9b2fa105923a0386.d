11bacb6ddbc3fb0008c00ffff1a0787d81845310
David Villafaña <david.villafana@capcu.org>
2024-04-04 12:01:33 -0500

    removed all but grammar and highlighting query

--- target/debug/deps/tree_sitter_poweron-9b2fa105923a0386.d
+++
@@ -1,6 +0,0 @@
-/Users/phil/projects/tree-sitter-poweron/target/debug/deps/tree_sitter_poweron-9b2fa105923a0386.rmeta: bindings/rust/lib.rs bindings/rust/../../src/node-types.json
-
-/Users/phil/projects/tree-sitter-poweron/target/debug/deps/tree_sitter_poweron-9b2fa105923a0386.d: bindings/rust/lib.rs bindings/rust/../../src/node-types.json
-
-bindings/rust/lib.rs:
-bindings/rust/../../src/node-types.json:
